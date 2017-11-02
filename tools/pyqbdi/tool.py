cbef313475e5dd40816892306517e47c66fa0cae
Jonathan Salwan <jonathan.salwan@gmail.com>
2017-11-02 16:15:15 +0100

    Add Python bindings for the QBDI library

---
+++ tools/pyqbdi/tool.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+## -*- coding: utf-8 -*-
+
+import pyqbdi
+
+def mycb(inst, gpr, fpr):
+    print "0x%x: %s" %(inst.address, inst.disassembly)
+    return pyqbdi.CONTINUE
+
+if __name__ == '__main__':
+    pyqbdi.addCodeCB(pyqbdi.PREINST, mycb)
