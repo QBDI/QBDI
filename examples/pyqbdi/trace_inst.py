2983e806aa7805396d9636efcb638d0da8d31f18
Jonathan Salwan <jonathan.salwan@gmail.com>
2017-11-20 16:43:39 +0100

    [pyqbdi] Review: Better warning and default arg for getInstAnalysis()

--- examples/pyqbdi/trace_inst.py
+++ examples/pyqbdi/trace_inst.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python2
-## -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import pyqbdi
 
+
 def mycb(vm, gpr, fpr):
-    inst = vm.getInstAnalysis(pyqbdi.ANALYSIS_INSTRUCTION | pyqbdi.ANALYSIS_DISASSEMBLY)
-    print "0x%x: %s" %(inst.address, inst.disassembly)
+    inst = vm.getInstAnalysis()
+    print "0x%x: %s" % (inst.address, inst.disassembly)
     return pyqbdi.CONTINUE
+
 
 def pyqbdipreload_on_run(vm, start, stop):
     vm.addCodeCB(pyqbdi.PREINST, mycb)
