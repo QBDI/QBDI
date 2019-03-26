54d844770e57b8e23898b898aa5f30695b51eb13
Nicolas SURBAYROLE <nsurbayrole@quarkslab.com>
2019-03-26 19:31:11 +0100

    Correct error on travis build with linux x64

--- examples/modules.c
+++ examples/modules.c
@@ -17,7 +17,7 @@ int main(int argc, char** argv) {
     free(modules);
 
     QBDI_MemoryMap *maps = qbdi_getCurrentProcessMaps(&size);
-    for(size_t i = 0; i < size; i++) {
+    for(i = 0; i < size; i++) {
         printf("%s (%d) ", maps[i].name, maps[i].permission);
         printf("(%#" PRIRWORD ", %#" PRIRWORD ")\n", maps[i].start, maps[i].end);
     }
