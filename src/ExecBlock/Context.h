ccf073fa37080cfe7510f00782d76b4eedcb5754
Nicolas SURBAYROLE <nsurbayrole@quarkslab.com>
2019-07-30 13:52:20 +0200

    Add flags in Context

--- src/ExecBlock/Context.h
+++ src/ExecBlock/Context.h
@@ -49,14 +49,14 @@ static const unsigned int GPR_ID[] = {
     llvm::X86::RSI, llvm::X86::RDI, llvm::X86::R8,  llvm::X86::R9,
     llvm::X86::R10, llvm::X86::R11, llvm::X86::R12, llvm::X86::R13,
     llvm::X86::R14, llvm::X86::R15, llvm::X86::RBP, llvm::X86::RSP,
-    llvm::X86::RIP
+    llvm::X86::RIP, llvm::X86::EFLAGS
 };
 
 #elif defined(QBDI_ARCH_X86)
 static const unsigned int GPR_ID[] = {
     llvm::X86::EAX, llvm::X86::EBX, llvm::X86::ECX, llvm::X86::EDX,
     llvm::X86::ESI, llvm::X86::EDI, llvm::X86::EBP, llvm::X86::ESP,
-    llvm::X86::EIP
+    llvm::X86::EIP, llvm::X86::EFLAGS
 };
 #endif
 
@@ -103,7 +103,8 @@ static const unsigned int GPR_ID[] = {
     llvm::ARM::R0, llvm::ARM::R1, llvm::ARM::R2,  llvm::ARM::R3,
     llvm::ARM::R4, llvm::ARM::R5, llvm::ARM::R6,  llvm::ARM::R7,
     llvm::ARM::R8, llvm::ARM::R9, llvm::ARM::R10, llvm::ARM::R12,
-    llvm::ARM::R11, llvm::ARM::SP, llvm::ARM::LR, llvm::ARM::PC
+    llvm::ARM::R11, llvm::ARM::SP, llvm::ARM::LR, llvm::ARM::PC,
+    llvm::ARM::CPSR
 };
 
 /*! ARM Host context.
