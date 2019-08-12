58e387f6aa6eefe1164e0c222394725fb1eab924
deesse-k <skaczmarek@quarkslab.com>
2019-08-12 11:06:33 +0200

    [compil] visual studio 14 to 16

--- cmake/config-win-X86_64.py
+++ cmake/config-win-X86_64.py
@@ -1,7 +1,8 @@
 import subprocess
 
 subprocess.check_call(["cmake", "..",
-                       "-G", "Visual Studio 14 2015 Win64",
+                       "-G", "Visual Studio 16 2019",
+                       "-A", "x64",
                        "-DCMAKE_BUILD_TYPE=Release",
                        "-DCMAKE_CROSSCOMPILING=FALSE",
                        "-DPLATFORM=win-X86_64"])
