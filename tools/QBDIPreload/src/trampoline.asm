ad221991ea9f8a572ce427ef74191d8e8e8df660
deesse-k <skaczmarek@quarkslab.com>
2019-08-19 09:56:04 +0200

    [winpreload] license headers & comments

--- tools/QBDIPreload/src/trampoline.asm
+++ tools/QBDIPreload/src/trampoline.asm
@@ -1,9 +1,36 @@
+; This file is part of QBDI.
+;
+; Copyright 2017 Quarkslab
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+
 .code
 
+; Assembly exported trampoline
 PUBLIC qbdipreload_trampoline 
+
+; C trampoline destination import
 EXTERN qbdipreload_trampoline_impl : proc
+
+; Original app RSP (stack top) value when
+; entering start (PE EntryPoint)
 EXTERN g_shadowStackTop : QWORD
 
+; Trampoline stub
+; Update RSP and jump to trampoline implementation
+; (in C), g_shadowStackTop must be intiialized before
+; using the trampoline
+; Note that RBP is not used on x64
 qbdipreload_trampoline PROC
     mov rsp, g_shadowStackTop
     jmp qbdipreload_trampoline_impl
