468f34db5ac3bfaa2e0e1bd7e0208f8b15f04e99
deesse-k <skaczmarek@quarkslab.com>
2019-09-02 13:12:08 +0200

    [winpreload] visual studio 2019 upgrade

--- cmake/config-win-X86.py
+++ cmake/config-win-X86.py
@@ -1,7 +1,8 @@
 import subprocess
 
 subprocess.check_call(["cmake", "..",
-                       "-G", "Visual Studio 14 2015",
+                       "-G", "Visual Studio 16 2019",
+                       "-A", "Win32",
                        "-DCMAKE_CXX_FLAGS=/D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING",
                        "-DCMAKE_BUILD_TYPE=Release",
                        "-DCMAKE_CROSSCOMPILING=FALSE",
