5bb80f26b19ed7a6dfa2a4b4fda6df474949a30c
Nicolas Surbayrole <nsurbayrole@quarkslab.com>
2021-03-29 10:44:09 +0200

    Format cmake files

---
+++ .cmake_format.conf.py
@@ -0,0 +1,8 @@
+with section("format"):
+
+  line_width = 80
+  tab_size = 2
+  use_tabchars = False
+
+with section("markup"):
+  enable_markup = False
--- cmake/spdlog.cmake
+++ cmake/spdlog.cmake
@@ -1,5 +1,5 @@
 if(__add_qbdi_spdlog)
-	return()
+  return()
 endif()
 set(__add_qbdi_spdlog ON)
 include(ExternalProject)
@@ -7,13 +7,15 @@ include(ExternalProject)
 # spdlog
 # ======
 set(SPDLOG_VERSION 1.8.3)
-set(SPDLOG_URL "https://github.com/gabime/spdlog/archive/refs/tags/v${SPDLOG_VERSION}.zip")
-ExternalProject_Add(spdlog-project
-  URL               ${SPDLOG_URL}
+set(SPDLOG_URL
+    "https://github.com/gabime/spdlog/archive/refs/tags/v${SPDLOG_VERSION}.zip")
+ExternalProject_Add(
+  spdlog-project
+  URL ${SPDLOG_URL}
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND     ""
-  INSTALL_COMMAND   "")
-ExternalProject_get_property(spdlog-project SOURCE_DIR)
+  BUILD_COMMAND ""
+  INSTALL_COMMAND "")
+ExternalProject_Get_Property(spdlog-project SOURCE_DIR)
 set(SPDLOG_SOURCE_DIR "${SOURCE_DIR}")
 
 add_library(spdlog INTERFACE)
