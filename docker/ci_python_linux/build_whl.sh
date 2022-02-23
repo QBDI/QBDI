288a0c23216dbd95ebc0f4a780c3e6e43e260a0d
Nicolas Surbayrole <nsurbayrole@quarkslab.com>
2022-02-23 16:40:32 +0100

    Update to python 3.10

--- docker/ci_python_linux/build_whl.sh
+++ docker/ci_python_linux/build_whl.sh
@@ -4,10 +4,9 @@ set -ex
 
 cd ~/qbdi
 
-/opt/python/cp36-cp36m/bin/python setup.py bdist_wheel
-/opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
 /opt/python/cp38-cp38/bin/python setup.py bdist_wheel
 /opt/python/cp39-cp39/bin/python setup.py bdist_wheel
+/opt/python/cp310-cp310/bin/python setup.py bdist_wheel
 
 for i in dist/*; do
     auditwheel repair $i -w outwheel;
