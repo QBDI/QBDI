f94b8e6b5d07cecaeaec8aef78816d95d7d923e3
Nicolas Surbayrole <nsurbayrole@quarkslab.com>
2022-11-15 11:54:35 +0100

    Fix PyQBDI windows CI

--- setup.py
+++ setup.py
@@ -101,8 +101,15 @@ class CMakeBuild(build_ext):
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
                                                               self.distribution.get_version())
+        for var in ["Python3_ROOT_DIR"]:
+            if var in env:
+                cmake_args.append("-D{}={}".format(var, env[var]))
+            elif var.upper() in env:
+                cmake_args.append("-D{}={}".format(var, env[var.upper()]))
+
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
+
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
         subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
