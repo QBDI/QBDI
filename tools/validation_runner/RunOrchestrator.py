85a68d1b8ef3990b24a1e4c837c2ff36c334ae73
Nicolas Surbayrole <nsurbayrole@quarkslab.com>
2023-01-25 11:43:49 +0100

    Update Copyright

--- tools/validation_runner/RunOrchestrator.py
+++ tools/validation_runner/RunOrchestrator.py
@@ -1,7 +1,7 @@
 #
 # This file is part of QBDI.
 #
-# Copyright 2017 - 2022 Quarkslab
+# Copyright 2017 - 2023 Quarkslab
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
