d690ec3380cb2cf728742646021e5f5a772447f9
Nicolas Surbayrole <nsurbayrole@quarkslab.com>
2024-02-29 16:26:35 +0100

    Fix compilation Android X86 with NDK r26

--- cmake/config/config-android-X86.sh
+++ cmake/config/config-android-X86.sh
@@ -20,7 +20,7 @@ fi
 
 cmake  .. \
       -DQBDI_PLATFORM=android \
-      -DQBDI_ARCH=X86_64 \
+      -DQBDI_ARCH=X86 \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_TOOLCHAIN_FILE="${NDK_PATH}/build/cmake/android.toolchain.cmake" \
       -DANDROID_ABI=x86 \
