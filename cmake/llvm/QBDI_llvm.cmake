687b808fceace338bed18b5e03dfa082eaada23b
Nicolas Surbayrole <nsurbayrole@quarkslab.com>
2024-07-24 15:25:49 +0200

    Refactor FetchContent usage

--- cmake/llvm/QBDI_llvm.cmake
+++ cmake/llvm/QBDI_llvm.cmake
@@ -3,7 +3,7 @@ if(__add_qbdi_llvm)
 endif()
 set(__add_qbdi_llvm ON)
 
-include(FetchContent)
+include(FetchContent_local)
 
 # configure FetchContent
 set(QBDI_LLVM_MAJOR_VERSION 17)
@@ -20,194 +20,193 @@ if(QBDI_INCLUDE_LLVM_CMAKE_MODUKE)
       SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/cmake"
     DOWNLOAD_DIR "${QBDI_THIRD_PARTY_DIRECTORY}/llvm-cmake-download")
 
-  if(NOT llvm_cmake_POPULATED)
-    FetchContent_Populate(llvm_cmake)
-  endif()
+  FetchContent_MakeAvailable(llvm_cmake)
 endif()
+
+set(QBDI_LLVM_URL
+    "https://github.com/llvm/llvm-project/releases/download/llvmorg-${QBDI_LLVM_VERSION}/llvm-${QBDI_LLVM_VERSION}.src.tar.xz"
+)
+set(QBDI_LLVM_URL_HASH
+    "SHA256=b638167da139126ca11917b6880207cc6e8f9d1cbb1a48d87d017f697ef78188")
 
 FetchContent_Declare(
   llvm
-  URL "https://github.com/llvm/llvm-project/releases/download/llvmorg-${QBDI_LLVM_VERSION}/llvm-${QBDI_LLVM_VERSION}.src.tar.xz"
-  URL_HASH
-    "SHA256=b638167da139126ca11917b6880207cc6e8f9d1cbb1a48d87d017f697ef78188"
-  DOWNLOAD_DIR "${QBDI_THIRD_PARTY_DIRECTORY}/llvm-download")
+  URL "${QBDI_LLVM_URL}"
+  URL_HASH "${QBDI_LLVM_URL_HASH}"
+  DOWNLOAD_DIR "${QBDI_THIRD_PARTY_DIRECTORY}/llvm-download"
+               ${FETCHCONTENT_EXCLUDE_FROM_ALL})
 
-FetchContent_GetProperties(llvm)
-if(NOT llvm_POPULATED)
-  FetchContent_Populate(llvm)
+set(CMAKE_CXX_STANDARD
+    17
+    CACHE INTERNAL "USE CPP 17")
+set(LLVM_BUILD_TOOLS
+    OFF
+    CACHE INTERNAL "Disable LLVM_BUILD_TOOLS")
+set(LLVM_BUILD_UTILS
+    OFF
+    CACHE INTERNAL "Disable LLVM_BUILD_UTILS")
+set(LLVM_BUILD_TESTS
+    OFF
+    CACHE INTERNAL "Disable LLVM_BUILD_TESTS")
+set(LLVM_BUILD_BENCHMARKS
+    OFF
+    CACHE INTERNAL "Disable LLVM_BUILD_BENCHMARKS")
+set(LLVM_BUILD_EXAMPLES
+    OFF
+    CACHE INTERNAL "Disable LLVM_BUILD_EXAMPLES")
+set(LLVM_INCLUDE_TOOLS
+    OFF
+    CACHE INTERNAL "Disable LLVM_INCLUDE_TOOLS")
+set(LLVM_INCLUDE_UTILS
+    OFF
+    CACHE INTERNAL "Disable LLVM_INCLUDE_UTILS")
+set(LLVM_INCLUDE_TESTS
+    OFF
+    CACHE INTERNAL "Disable LLVM_INCLUDE_TESTS")
+set(LLVM_INCLUDE_BENCHMARKS
+    OFF
+    CACHE INTERNAL "Disable LLVM_INCLUDE_BENCHMARKS")
+set(LLVM_INCLUDE_EXAMPLES
+    OFF
+    CACHE INTERNAL "Disable LLVM_INCLUDE_EXAMPLES")
+set(LLVM_ENABLE_TERMINFO
+    OFF
+    CACHE INTERNAL "Disable LLVM_ENABLE_TERMINFO")
+set(LLVM_ENABLE_BINDINGS
+    OFF
+    CACHE INTERNAL "Disable LLVM_ENABLE_BINDINGS")
+set(LLVM_ENABLE_RTTI
+    OFF
+    CACHE INTERNAL "Disable LLVM_ENABLE_RTTI")
+set(LLVM_APPEND_VC_REV
+    OFF
+    CACHE INTERNAL "Disable LLVM_APPEND_VC_REV")
+set(LLVM_ENABLE_Z3_SOLVER
+    OFF
+    CACHE INTERNAL "Disable LLVM_ENABLE_Z3_SOLVER")
+set(LLVM_ENABLE_ZLIB
+    OFF
+    CACHE INTERNAL "Disable LLVM_ENABLE_ZLIB")
+set(LLVM_ENABLE_ZSTD
+    OFF
+    CACHE INTERNAL "Disable LLVM_ENABLE_ZSTD")
+set(LLVM_TARGET_ARCH
+    ${QBDI_LLVM_ARCH}
+    CACHE INTERNAL "set LLVM_ARCH")
+set(LLVM_TARGETS_TO_BUILD
+    ${QBDI_LLVM_ARCH}
+    CACHE INTERNAL "set LLVM_TARGETS_TO_BUILD")
 
-  set(CMAKE_CXX_STANDARD
-      17
-      CACHE STRING "USE CPP 17")
-  set(LLVM_BUILD_TOOLS
-      OFF
-      CACHE BOOL "Disable LLVM_BUILD_TOOLS")
-  set(LLVM_BUILD_UTILS
-      OFF
-      CACHE BOOL "Disable LLVM_BUILD_UTILS")
-  set(LLVM_BUILD_TESTS
-      OFF
-      CACHE BOOL "Disable LLVM_BUILD_TESTS")
-  set(LLVM_BUILD_BENCHMARKS
-      OFF
-      CACHE BOOL "Disable LLVM_BUILD_BENCHMARKS")
-  set(LLVM_BUILD_EXAMPLES
-      OFF
-      CACHE BOOL "Disable LLVM_BUILD_EXAMPLES")
-  set(LLVM_INCLUDE_TOOLS
-      OFF
-      CACHE BOOL "Disable LLVM_INCLUDE_TOOLS")
-  set(LLVM_INCLUDE_UTILS
-      OFF
-      CACHE BOOL "Disable LLVM_INCLUDE_UTILS")
-  set(LLVM_INCLUDE_TESTS
-      OFF
-      CACHE BOOL "Disable LLVM_INCLUDE_TESTS")
-  set(LLVM_INCLUDE_BENCHMARKS
-      OFF
-      CACHE BOOL "Disable LLVM_INCLUDE_BENCHMARKS")
-  set(LLVM_INCLUDE_EXAMPLES
-      OFF
-      CACHE BOOL "Disable LLVM_INCLUDE_EXAMPLES")
-  set(LLVM_ENABLE_TERMINFO
-      OFF
-      CACHE BOOL "Disable LLVM_ENABLE_TERMINFO")
-  set(LLVM_ENABLE_BINDINGS
-      OFF
-      CACHE BOOL "Disable LLVM_ENABLE_BINDINGS")
-  set(LLVM_ENABLE_RTTI
-      OFF
-      CACHE BOOL "Disable LLVM_ENABLE_RTTI")
-  set(LLVM_APPEND_VC_REV
-      OFF
-      CACHE BOOL "Disable LLVM_APPEND_VC_REV")
-  set(LLVM_ENABLE_Z3_SOLVER
-      OFF
-      CACHE BOOL "Disable LLVM_ENABLE_Z3_SOLVER")
-  set(LLVM_ENABLE_ZLIB
-      OFF
-      CACHE BOOL "Disable LLVM_ENABLE_ZLIB")
-  set(LLVM_ENABLE_ZSTD
-      OFF
-      CACHE BOOL "Disable LLVM_ENABLE_ZSTD")
-  set(LLVM_TARGET_ARCH
-      ${QBDI_LLVM_ARCH}
-      CACHE STRING "set LLVM_ARCH")
-  set(LLVM_TARGETS_TO_BUILD
-      ${QBDI_LLVM_ARCH}
-      CACHE STRING "set LLVM_TARGETS_TO_BUILD")
-
-  set(QBDI_LLVM_TRIPLE "")
-  if(QBDI_ARCH_ARM)
-    if(QBDI_PLATFORM_ANDROID)
-      set(QBDI_LLVM_TRIPLE armv7-linux-androideabi)
-    else()
-      set(QBDI_LLVM_TRIPLE armv7-linux-gnu)
-    endif()
-  elseif(QBDI_ARCH_AARCH64)
-    if(QBDI_PLATFORM_ANDROID)
-      set(QBDI_LLVM_TRIPLE aarch64-linux-android)
-    else()
-      set(QBDI_LLVM_TRIPLE aarch64-linux-gnu)
-    endif()
-  elseif(QBDI_ARCH_X86)
-    set(LLVM_BUILD_32_BITS
-        ON
-        CACHE BOOL "set LLVM_BUILD_32_BITS")
-
-    if(QBDI_PLATFORM_OSX)
-      set(LLVM_ENABLE_LIBCXX
-          ON
-          CACHE BOOL "set LLVM_ENABLE_LIBCXX")
-      set(QBDI_LLVM_TRIPLE i386-apple-darwin17.7.0)
-    elseif(QBDI_PLATFORM_LINUX OR QBDI_PLATFORM_ANDROID)
-      set(QBDI_LLVM_TRIPLE i386-pc-linux)
-    endif()
-
-  elseif(QBDI_ARCH_X86_64)
-    if(QBDI_PLATFORM_LINUX)
-      set(QBDI_LLVM_TRIPLE x86_64-pc-linux-gnu)
-    endif()
+set(QBDI_LLVM_TRIPLE "")
+if(QBDI_ARCH_ARM)
+  if(QBDI_PLATFORM_ANDROID)
+    set(QBDI_LLVM_TRIPLE armv7-linux-androideabi)
   else()
-    message(FATAL_ERROR "Unsupported LLVM Architecture.")
+    set(QBDI_LLVM_TRIPLE armv7-linux-gnu)
+  endif()
+elseif(QBDI_ARCH_AARCH64)
+  if(QBDI_PLATFORM_ANDROID)
+    set(QBDI_LLVM_TRIPLE aarch64-linux-android)
+  else()
+    set(QBDI_LLVM_TRIPLE aarch64-linux-gnu)
+  endif()
+elseif(QBDI_ARCH_X86)
+  set(LLVM_BUILD_32_BITS
+      ON
+      CACHE INTERNAL "set LLVM_BUILD_32_BITS")
+
+  if(QBDI_PLATFORM_OSX)
+    set(LLVM_ENABLE_LIBCXX
+        ON
+        CACHE INTERNAL "set LLVM_ENABLE_LIBCXX")
+    set(QBDI_LLVM_TRIPLE i386-apple-darwin17.7.0)
+  elseif(QBDI_PLATFORM_LINUX OR QBDI_PLATFORM_ANDROID)
+    set(QBDI_LLVM_TRIPLE i386-pc-linux)
   endif()
 
-  if(NOT ("${QBDI_LLVM_TRIPLE}" STREQUAL ""))
-    set(LLVM_DEFAULT_TARGET_TRIPLE
-        "${QBDI_LLVM_TRIPLE}"
-        CACHE STRING "set LLVM_DEFAULT_TARGET_TRIPLE")
+elseif(QBDI_ARCH_X86_64)
+  if(QBDI_PLATFORM_LINUX)
+    set(QBDI_LLVM_TRIPLE x86_64-pc-linux-gnu)
   endif()
+else()
+  message(FATAL_ERROR "Unsupported LLVM Architecture.")
+endif()
 
-  # build llvm with visibility hidden
-  if(DEFINED CMAKE_C_VISIBILITY_PRESET)
-    set(QBDI_CACHE_CMAKE_C_VISIBILITY_PRESET ${CMAKE_C_VISIBILITY_PRESET})
+if(NOT ("${QBDI_LLVM_TRIPLE}" STREQUAL ""))
+  set(LLVM_DEFAULT_TARGET_TRIPLE
+      "${QBDI_LLVM_TRIPLE}"
+      CACHE INTERNAL "set LLVM_DEFAULT_TARGET_TRIPLE")
+endif()
+
+# build llvm with visibility hidden
+if(DEFINED CMAKE_C_VISIBILITY_PRESET)
+  set(QBDI_CACHE_CMAKE_C_VISIBILITY_PRESET ${CMAKE_C_VISIBILITY_PRESET})
+endif()
+if(DEFINED CMAKE_CXX_VISIBILITY_PRESET)
+  set(QBDI_CACHE_CMAKE_CXX_VISIBILITY_PRESET ${CMAKE_CXX_VISIBILITY_PRESET})
+endif()
+set(CMAKE_C_VISIBILITY_PRESET
+    "hidden"
+    CACHE STRING "set CMAKE_C_VISIBILITY_PRESET" FORCE)
+set(CMAKE_CXX_VISIBILITY_PRESET
+    "hidden"
+    CACHE STRING "set CMAKE_CXX_VISIBILITY_PRESET" FORCE)
+
+if(NOT ("${NATIVE_TABLEN_PATH}" STREQUAL ""))
+  set(LLVM_TABLEGEN
+      "${NATIVE_TABLEN_PATH}"
+      CACHE INTERNAL "force tablegen")
+elseif(NOT ("${QBDI_LLVM_TABLEN_TOOLSCHAIN}" STREQUAL ""))
+  # create a second directory to build the native llvm-tblgen
+  # mostly use when crosscompile and need another compiler to create a native
+  # target
+  include(QBDI_llvm_tblgen)
+  set(LLVM_TABLEGEN
+      "${QBDI_LLVM_NATIVE_TBLGEN}"
+      CACHE INTERNAL "force tablegen")
+else()
+  # check if llvm-tblgen-X is available
+  find_program(LLVM_TABLEN_BIN NAMES llvm-tblgen-${QBDI_LLVM_MAJOR_VERSION})
+  message(STATUS "LLVM Table Gen found: ${LLVM_TABLEN_BIN}")
+  if(${LLVM_TABLEN_BIN_FOUND})
+    set(LLVM_TABLEGEN
+        "${LLVM_TABLEN_BIN}"
+        CACHE INTERNAL "force tablegen")
   endif()
-  if(DEFINED CMAKE_CXX_VISIBILITY_PRESET)
-    set(QBDI_CACHE_CMAKE_CXX_VISIBILITY_PRESET ${CMAKE_CXX_VISIBILITY_PRESET})
-  endif()
+endif()
+
+if(QBDI_CCACHE AND CCACHE_FOUND)
+  set(LLVM_CCACHE_BUILD
+      ON
+      CACHE INTERNAL "Enable CCACHE in llvm")
+else()
+  set(LLVM_CCACHE_BUILD
+      OFF
+      CACHE INTERNAL "Enable CCACHE in llvm")
+endif()
+
+if(QBDI_ASAN AND HAVE_FLAG_SANITIZE_ADDRESS)
+  set(LLVM_USE_SANITIZER
+      Address
+      CACHE INTERNAL "Enable ASAN")
+endif()
+
+fetchcontent_makeavailable_exclude_from_all(llvm)
+
+# restore visibility
+if(DEFINED QBDI_CACHE_CMAKE_C_VISIBILITY_PRESET)
   set(CMAKE_C_VISIBILITY_PRESET
-      "hidden"
+      ${QBDI_CACHE_CMAKE_C_VISIBILITY_PRESET}
       CACHE STRING "set CMAKE_C_VISIBILITY_PRESET" FORCE)
+else()
+  unset(CMAKE_C_VISIBILITY_PRESET CACHE)
+endif()
+if(DEFINED QBDI_CACHE_CMAKE_CXX_VISIBILITY_PRESET)
   set(CMAKE_CXX_VISIBILITY_PRESET
-      "hidden"
+      ${QBDI_CACHE_CMAKE_CXX_VISIBILITY_PRESET}
       CACHE STRING "set CMAKE_CXX_VISIBILITY_PRESET" FORCE)
-
-  if(NOT ("${NATIVE_TABLEN_PATH}" STREQUAL ""))
-    set(LLVM_TABLEGEN
-        "${NATIVE_TABLEN_PATH}"
-        CACHE STRING "force tablegen")
-  elseif(NOT ("${QBDI_LLVM_TABLEN_TOOLSCHAIN}" STREQUAL ""))
-    # create a second directory to build the native llvm-tblgen
-    # mostly use when crosscompile and need another compiler to create a native
-    # target
-    include(QBDI_llvm_tblgen)
-    set(LLVM_TABLEGEN
-        "${QBDI_LLVM_NATIVE_TBLGEN}"
-        CACHE STRING "force tablegen")
-  else()
-    # check if llvm-tblgen-X is available
-    find_program(LLVM_TABLEN_BIN NAMES llvm-tblgen-${QBDI_LLVM_MAJOR_VERSION})
-    message(STATUS "LLVM Table Gen found: ${LLVM_TABLEN_BIN}")
-    if(${LLVM_TABLEN_BIN_FOUND})
-      set(LLVM_TABLEGEN
-          "${LLVM_TABLEN_BIN}"
-          CACHE STRING "force tablegen")
-    endif()
-  endif()
-
-  if(QBDI_CCACHE AND CCACHE_FOUND)
-    set(LLVM_CCACHE_BUILD
-        ON
-        CACHE BOOL "Enable CCACHE in llvm")
-  else()
-    set(LLVM_CCACHE_BUILD
-        OFF
-        CACHE BOOL "Enable CCACHE in llvm")
-  endif()
-
-  if(QBDI_ASAN AND HAVE_FLAG_SANITIZE_ADDRESS)
-    set(LLVM_USE_SANITIZER
-        Address
-        CACHE STRING "Enable ASAN")
-  endif()
-
-  add_subdirectory(${llvm_SOURCE_DIR} ${llvm_BINARY_DIR} EXCLUDE_FROM_ALL)
-
-  # restore visibility
-  if(DEFINED QBDI_CACHE_CMAKE_C_VISIBILITY_PRESET)
-    set(CMAKE_C_VISIBILITY_PRESET
-        ${QBDI_CACHE_CMAKE_C_VISIBILITY_PRESET}
-        CACHE STRING "set CMAKE_C_VISIBILITY_PRESET" FORCE)
-  else()
-    unset(CMAKE_C_VISIBILITY_PRESET CACHE)
-  endif()
-  if(DEFINED QBDI_CACHE_CMAKE_CXX_VISIBILITY_PRESET)
-    set(CMAKE_CXX_VISIBILITY_PRESET
-        ${QBDI_CACHE_CMAKE_CXX_VISIBILITY_PRESET}
-        CACHE STRING "set CMAKE_CXX_VISIBILITY_PRESET" FORCE)
-  else()
-    unset(CMAKE_CXX_VISIBILITY_PRESET CACHE)
-  endif()
+else()
+  unset(CMAKE_CXX_VISIBILITY_PRESET CACHE)
 endif()
 
 # list of LLVM library to build
