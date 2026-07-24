qaqa02402/QBDI-android-art-stack-fix

batch2 X86 memory access

test/API/X86/InstructionExtended/StackFrame_X86.cpp
@@ -1,0 +1,80 @@
+/*
+ * This file is part of QBDI.
+ *
+ * Copyright 2017 - 2026 Quarkslab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "MemAccessTestUtils_X86.h"
+
+using QBDITestBatch2::checkAccess;
+using QBDITestBatch2::ExpectedMemoryAccess;
+using QBDITestBatch2::ExpectedMemoryAccesses;
+
+TEST_CASE_METHOD(APITest, "InstructionExtendedTest_X86-ENTER") {
+  const char source[] =
+      "xchg %esp, %edx\n"
+      "enter $8, $0\n"
+      "leave\n"
+      "xchg %esp, %edx\n";
+  QBDI::rword tmpStack[10] = {0};
+  QBDI::rword stackAddr = (QBDI::rword)&tmpStack[9] - 4;
+  constexpr uint32_t oldEbp = 0x99aabbcc;
+  ExpectedMemoryAccesses expectedPre = {{}};
+  ExpectedMemoryAccesses expectedPost = {{
+      {stackAddr, oldEbp, 4, QBDI::MEMORY_WRITE, QBDI::MEMORY_NO_FLAGS},
+  }};
+  vm.recordMemoryAccess(QBDI::MEMORY_READ_WRITE);
+  vm.addMnemonicCB("ENTER", QBDI::PREINST, checkAccess, &expectedPre);
+  vm.addMnemonicCB("ENTER", QBDI::POSTINST, checkAccess, &expectedPost);
+  QBDI::GPRState *state = vm.getGPRState();
+  state->ebp = oldEbp;
+  state->edx = (QBDI::rword)&tmpStack[9];
+  vm.setGPRState(state);
+  QBDI::rword retval;
+  bool ran = runOnASM(&retval, source);
+  CHECK(ran);
+  for (auto &e : expectedPost.accesses)
+    CHECK(e.see);
+}
+
+TEST_CASE_METHOD(APITest, "InstructionExtendedTest_X86-LEAVE") {
+  const char source[] =
+      "xchg %esp, %edx\n"
+      "leave\n"
+      "xchg %esp, %edx\n";
+  QBDI::rword tmpStack[10] = {0};
+  uint32_t *savedEbpSlot = reinterpret_cast<uint32_t *>(&tmpStack[8]);
+  constexpr uint32_t savedEbp = 0x11223344;
+  *savedEbpSlot = savedEbp;
+  QBDI::rword ebpAddr = (QBDI::rword)savedEbpSlot;
+  ExpectedMemoryAccesses expectedPre = {{
+      {ebpAddr, savedEbp, 4, QBDI::MEMORY_READ, QBDI::MEMORY_NO_FLAGS},
+  }};
+  ExpectedMemoryAccesses expectedPost = expectedPre;
+  vm.recordMemoryAccess(QBDI::MEMORY_READ_WRITE);
+  vm.addMnemonicCB("LEAVE", QBDI::PREINST, checkAccess, &expectedPre);
+  vm.addMnemonicCB("LEAVE", QBDI::POSTINST, checkAccess, &expectedPost);
+  QBDI::GPRState *state = vm.getGPRState();
+  state->ebp = ebpAddr;
+  state->edx = (QBDI::rword)&tmpStack[9];
+  vm.setGPRState(state);
+  QBDI::rword retval;
+  bool ran = runOnASM(&retval, source);
+  CHECK(ran);
+  for (auto &e : expectedPre.accesses)
+    CHECK(e.see);
+  for (auto &e : expectedPost.accesses)
+    CHECK(e.see);
+}

test/API/X86/InstructionExtended/X87Float864_X86.cpp
@@ -1,0 +1,69 @@
+/*
+ * This file is part of QBDI.
+ *
+ * Copyright 2017 - 2026 Quarkslab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "MemAccessTestUtils_X86.h"
+
+using QBDITestBatch2::checkAccess;
+using QBDITestBatch2::ExpectedMemoryAccess;
+using QBDITestBatch2::ExpectedMemoryAccesses;
+
+TEST_CASE_METHOD(APITest, "InstructionExtendedTest_X86-FRSTORm") {
+  const char source[] = "frstor 0x11(%ebx,%esi,4)\n";
+  uint8_t buffer[160] = {0};
+  QBDI::rword targetAddr = (QBDI::rword)&buffer[21];
+  ExpectedMemoryAccesses expectedPre = {{
+      {targetAddr, 0, 108, QBDI::MEMORY_READ, QBDI::MEMORY_UNKNOWN_VALUE},
+  }};
+  ExpectedMemoryAccesses expectedPost = {{
+      {targetAddr, 0, 108, QBDI::MEMORY_READ, QBDI::MEMORY_UNKNOWN_VALUE},
+  }};
+  vm.recordMemoryAccess(QBDI::MEMORY_READ_WRITE);
+  vm.addMnemonicCB("FRSTORm", QBDI::PREINST, checkAccess, &expectedPre);
+  vm.addMnemonicCB("FRSTORm", QBDI::POSTINST, checkAccess, &expectedPost);
+  QBDI::GPRState *state = vm.getGPRState();
+  state->ebx = (QBDI::rword)&buffer[0];
+  state->esi = 1;
+  vm.setGPRState(state);
+  QBDI::rword retval;
+  bool ran = runOnASM(&retval, source);
+  CHECK(ran);
+  for (auto &e : expectedPre.accesses)
+    CHECK(e.see);
+  for (auto &e : expectedPost.accesses)
+    CHECK(e.see);
+}
+
+TEST_CASE_METHOD(APITest, "InstructionExtendedTest_X86-FSAVEm") {
+  const char source[] = "fnsave 0x11(%ebx,%esi,4)\n";
+  uint8_t buffer[160] = {0};
+  QBDI::rword targetAddr = (QBDI::rword)&buffer[21];
+  ExpectedMemoryAccesses expectedPost = {{
+      {targetAddr, 0, 108, QBDI::MEMORY_WRITE, QBDI::MEMORY_UNKNOWN_VALUE},
+  }};
+  vm.recordMemoryAccess(QBDI::MEMORY_READ_WRITE);
+  vm.addMnemonicCB("FSAVEm", QBDI::POSTINST, checkAccess, &expectedPost);
+  QBDI::GPRState *state = vm.getGPRState();
+  state->ebx = (QBDI::rword)&buffer[0];
+  state->esi = 1;
+  vm.setGPRState(state);
+  QBDI::rword retval;
+  bool ran = runOnASM(&retval, source);
+  CHECK(ran);
+  for (auto &e : expectedPost.accesses)
+    CHECK(e.see);
+}

QBDI/QBDI

test/API/X86/InstructionExtended/StackFrame_X86.cpp
@@ -1,0 +1,78 @@
+/*
+ * This file is part of QBDI.
+ *
+ * Copyright 2017 - 2026 Quarkslab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "MemAccessTestUtils_X86.h"
+
+using QBDITestBatch2::checkAccess;
+using QBDITestBatch2::ExpectedMemoryAccess;
+using QBDITestBatch2::ExpectedMemoryAccesses;
+
+TEST_CASE_METHOD(APITest, "InstructionExtendedTest_X86-ENTER") {
+  const char source[] =
+      "xchg %esp, %edx\n"
+      "enter $8, $0\n"
+      "leave\n"
+      "xchg %esp, %edx\n";
+  QBDI::rword tmpStack[10] = {0};
+  QBDI::rword stackAddr = (QBDI::rword)&tmpStack[9] - 4;
+  constexpr uint32_t oldEbp = 0x99aabbcc;
+  ExpectedMemoryAccesses expectedPre = {{}};
+  ExpectedMemoryAccesses expectedPost = {{
+      {stackAddr, oldEbp, 4, QBDI::MEMORY_WRITE, QBDI::MEMORY_NO_FLAGS},
+  }};
+  vm.recordMemoryAccess(QBDI::MEMORY_READ_WRITE);
+  vm.addMnemonicCB("ENTER", QBDI::PREINST, checkAccess, &expectedPre);
+  vm.addMnemonicCB("ENTER", QBDI::POSTINST, checkAccess, &expectedPost);
+  QBDI::GPRState *state = vm.getGPRState();
+  state->ebp = oldEbp;
+  state->edx = (QBDI::rword)&tmpStack[9];
+  vm.setGPRState(state);
+  QBDI::rword retval;
+  bool ran = runOnASM(&retval, source);
+  CHECK(ran);
+  CHECK(expectedPre.see);
+  CHECK(expectedPost.see);
+}
+
+TEST_CASE_METHOD(APITest, "InstructionExtendedTest_X86-LEAVE") {
+  const char source[] =
+      "xchg %esp, %edx\n"
+      "leave\n"
+      "xchg %esp, %edx\n";
+  QBDI::rword tmpStack[10] = {0};
+  uint32_t *savedEbpSlot = reinterpret_cast<uint32_t *>(&tmpStack[8]);
+  constexpr uint32_t savedEbp = 0x11223344;
+  *savedEbpSlot = savedEbp;
+  QBDI::rword ebpAddr = (QBDI::rword)savedEbpSlot;
+  ExpectedMemoryAccesses expectedPre = {{
+      {ebpAddr, savedEbp, 4, QBDI::MEMORY_READ, QBDI::MEMORY_NO_FLAGS},
+  }};
+  ExpectedMemoryAccesses expectedPost = expectedPre;
+  vm.recordMemoryAccess(QBDI::MEMORY_READ_WRITE);
+  vm.addMnemonicCB("LEAVE", QBDI::PREINST, checkAccess, &expectedPre);
+  vm.addMnemonicCB("LEAVE", QBDI::POSTINST, checkAccess, &expectedPost);
+  QBDI::GPRState *state = vm.getGPRState();
+  state->ebp = ebpAddr;
+  state->edx = (QBDI::rword)&tmpStack[9];
+  vm.setGPRState(state);
+  QBDI::rword retval;
+  bool ran = runOnASM(&retval, source);
+  CHECK(ran);
+  CHECK(expectedPre.see);
+  CHECK(expectedPost.see);
+}

test/API/X86/InstructionExtended/X87Float864_X86.cpp
@@ -1,0 +1,66 @@
+/*
+ * This file is part of QBDI.
+ *
+ * Copyright 2017 - 2026 Quarkslab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "MemAccessTestUtils_X86.h"
+
+using QBDITestBatch2::checkAccess;
+using QBDITestBatch2::ExpectedMemoryAccess;
+using QBDITestBatch2::ExpectedMemoryAccesses;
+
+TEST_CASE_METHOD(APITest, "InstructionExtendedTest_X86-FRSTORm") {
+  const char source[] = "frstor 0x11(%ebx,%esi,4)\n";
+  uint8_t buffer[160] = {0};
+  QBDI::rword targetAddr = (QBDI::rword)&buffer[21];
+  ExpectedMemoryAccesses expectedPre = {{
+      {targetAddr, 0, 108, QBDI::MEMORY_READ, QBDI::MEMORY_UNKNOWN_VALUE},
+  }};
+  ExpectedMemoryAccesses expectedPost = {{
+      {targetAddr, 0, 108, QBDI::MEMORY_READ, QBDI::MEMORY_UNKNOWN_VALUE},
+  }};
+  vm.recordMemoryAccess(QBDI::MEMORY_READ_WRITE);
+  vm.addMnemonicCB("FRSTORm", QBDI::PREINST, checkAccess, &expectedPre);
+  vm.addMnemonicCB("FRSTORm", QBDI::POSTINST, checkAccess, &expectedPost);
+  QBDI::GPRState *state = vm.getGPRState();
+  state->ebx = (QBDI::rword)&buffer[0];
+  state->esi = 1;
+  vm.setGPRState(state);
+  QBDI::rword retval;
+  bool ran = runOnASM(&retval, source);
+  CHECK(ran);
+  CHECK(expectedPre.see);
+  CHECK(expectedPost.see);
+}
+
+TEST_CASE_METHOD(APITest, "InstructionExtendedTest_X86-FSAVEm") {
+  const char source[] = "fnsave 0x11(%ebx,%esi,4)\n";
+  uint8_t buffer[160] = {0};
+  QBDI::rword targetAddr = (QBDI::rword)&buffer[21];
+  ExpectedMemoryAccesses expectedPost = {{
+      {targetAddr, 0, 108, QBDI::MEMORY_WRITE, QBDI::MEMORY_UNKNOWN_VALUE},
+  }};
+  vm.recordMemoryAccess(QBDI::MEMORY_READ_WRITE);
+  vm.addMnemonicCB("FSAVEm", QBDI::POSTINST, checkAccess, &expectedPost);
+  QBDI::GPRState *state = vm.getGPRState();
+  state->ebx = (QBDI::rword)&buffer[0];
+  state->esi = 1;
+  vm.setGPRState(state);
+  QBDI::rword retval;
+  bool ran = runOnASM(&retval, source);
+  CHECK(ran);
+  CHECK(expectedPost.see);
+}